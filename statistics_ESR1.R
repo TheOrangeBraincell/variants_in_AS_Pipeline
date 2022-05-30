355783ea1e79238fc7121906f8430f1f3aa37a7e
Mirjam Karlsson-Müller <90756078+MirjamKarlssonMueller@users.noreply.github.com>
2022-05-30 16:11:52 +0200

    Add files via upload
    minor changes to R script. Added output file.

--- statistics_ESR1.R
+++ statistics_ESR1.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 #
 # name: statistics_ESR1.R
 # author: Mirjam Karlsson-Müller
@@ -8,17 +9,39 @@
 # significant difference in p-values between genotypes within a potential 
 # cassette exon.
 # 
+# useage: Can be run in command line
+#     Rscript --vanilla statistics_ESR1.R CE_ESR1_all.txt genotypes_in_exons.txt
 
-#Import library
+# 0. Import library and set up inputs----
 library(tidyverse)
+library(optparse)
 
+#Console input
+option_list = list(
+  make_option(c("-p", "--psi"), type="character", default=NULL, 
+              help="PSI score table file name.", metavar="character"),
+  make_option(c("-g", "--genotype"), type="character", default=NULL, 
+              help="genotype table file name.", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="p_value.txt", 
+              help="output file name [default= %default]", metavar="character")
+); 
 
-#Change working directory based on where you have saved your output files.
-setwd("C:\\Users\\mirja\\Documents\\University\\ResearchProject\\Statistics")
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+#Check that both input files are given
+if (is.null(opt$psi)){
+  print_help(opt_parser)
+  stop("The psi score table must be provided as input with -p.", call.=FALSE)
+}
+if (is.null(opt$genotype)){
+  print_help(opt_parser)
+  stop("The genotype table must be provided as input with -g.", call.=FALSE)
+}
 
 # 1. Read in data -------
 #Psi score table
-psi_raw <- read_tsv("CE_ESR1_all.txt", comment = "#")
+psi_raw <- read_tsv(opt$psi, comment = "#")
 
 psi<-psi_raw %>% 
   pivot_longer(cols = -Location, names_to = "Sample", values_to = "psi")
@@ -31,7 +54,7 @@ psi<-psi %>%
 #psi
 
 #genotype table
-genotype_raw<-read_tsv("genotypes_in_exons.txt", comment="#")
+genotype_raw<-read_tsv(opt$genotype, comment="#")
 #genotype_raw
 
 genotype<-genotype_raw %>% 
@@ -96,3 +119,7 @@ p_frame$p_adjust<-p.adjust(c(p_frame$pvalue), method="bonferroni", n=length(c(p_
 p_frame
 #all 1's.
 
+#write p-values and adjusted p-values in output file
+sink(opt$out)
+cat(p_frame)
+sink()
