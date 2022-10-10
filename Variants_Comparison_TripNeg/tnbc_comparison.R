6d540ca85f9db539da855f5e9a67ee3e985f6a66
Mirjam Karlsson-Müller <90756078+MirjamKarlssonMueller@users.noreply.github.com>
2022-10-10 11:29:16 +0200

    Add files via upload

--- Variants_Comparison_TripNeg/tnbc_comparison.R
+++ Variants_Comparison_TripNeg/tnbc_comparison.R
@@ -11,7 +11,7 @@ library(tidyverse)
 
 setwd("C:\\Users\\mirja\\Documents\\University\\Master Thesis\\R and outputs\\TNBC_comparison")
 
-dna_variants<- read_tsv("tnbc_variants.tsv")
+dna_variants<- read_tsv("tnbc_variants_fpkm.tsv")
 rna_variants_filtered<-read_tsv("rna_variants_filtered.txt")
 rna_variants_unfiltered<-read_tsv("rna_variants.txt")
 
@@ -40,12 +40,16 @@ dna_variants %>%
 rna_variants_filtered %>% 
   filter(Sample %in% dna_samples)->rna_vfu #rna variants filtered updated
 
+rm(rna_variants_filtered)
+
 rna_variants_unfiltered %>% 
   filter(Sample %in% dna_samples)->rna_vu
+rm(rna_variants_unfiltered)
 
 dna_variants %>% 
   filter(Sample %in% rna_samples)->dna_vu #dna variants updated
 
+rm(dna_variants)
 
 #Only focus on single base substitutions at this point:
 # so exclude all lines with alt or ref longer than 1 char.
@@ -97,12 +101,13 @@ rnaf_exons %>%
 
 #common number between no germline and dna?
 rnaf_exons_no_germ<-rnaf_exons %>% 
-  filter(dbsnp=="No")
+  filter(dbsnp=="No") %>% 
+  select(!dbsnp)
 
 rnaf_exons_no_germ #117 624
 
-common_wo_germline_exon<-inner_join(dna_exons, rnaf_exons_no_germ)
-common_wo_germline_exon
+common_wo_germline_exonf<-inner_join(dna_exons, rnaf_exons_no_germ)
+common_wo_germline_exonf
 #4417
 #So most matches are not germline variants. which makes sense given that
 # germline variants are excluded from the dna data set (mostly.)
@@ -110,12 +115,118 @@ common_wo_germline_exon
 rna_exons_no_germ<-rna_exons %>% 
   filter(dbsnp=="No")
 
-rna_exons_no_germ #117 624
+rna_exons_no_germ #2 756 494
 
 common_wo_germline_exon<-inner_join(dna_exons, rna_exons_no_germ)
 common_wo_germline_exon
 
 
 #Relationship to Expression level?
+#using filtered data first
+rnaf_exons_no_germ %>% 
+  mutate(Match="T")->rnaf_exons_no_germ
+
+dna_exons %>% 
+  left_join(rnaf_exons_no_germ) %>%
+  mutate(FPKM=as.numeric(FPKM)) %>% 
+  mutate_at(vars(Match), ~replace_na(., "F")) %>% 
+  drop_na(FPKM)->dna_exons_rnaf
+
+dna_exons_rnaf
+
+dna_exons_rnaf$Match<-as.factor(dna_exons_rnaf$Match)
+#maximum_FPKM<-max(dna_exons$FPKM)
+dna_exons_rnaf$bins<-cut(dna_exons_rnaf$FPKM, breaks=seq(from=0, to=ceiling(max(dna_exons_rnaf$FPKM)), by=500))
+
+#dna_exons %>% 
+#  mutate(bins=cut(FPKM, breaks=seq(from=0, to=ceiling(max(FPKM)), by=500)))
+
+#Not percentages.
+dna_exons_rnaf %>% 
+  drop_na(FPKM) %>%
+  drop_na(bins) %>% 
+  ggplot(aes(fill=Match, x=bins))+
+  xlab("ranges of FPKM values")+
+  ylab("Number of variants")+
+  scale_fill_discrete(name = "Variant found in", labels = c("DNA", "DNA + RNA"))+
+  ggtitle("Variants found in DNA vs variants found in both DNA and RNA")+
+  #xlim(-1,501)+
+  geom_bar()
 
 
+#Only look at counts for fpkm <500
+
+dna_exons_rnaf_filtered<- dna_exons_rnaf %>%
+  select(!bins) %>% 
+  filter(FPKM<=500)
+
+dna_exons_rnaf_filtered$bins<-cut(dna_exons_rnaf_filtered$FPKM, breaks=seq(from=0, to=500, by=50))
+#dna_exons_filtered
+dna_exons_rnaf_filtered %>% 
+  drop_na(bins)->dna_exons_rnaf_filtered_no_na
+
+ggplot(dna_exons_rnaf_filtered_no_na, aes(fill=Match, x=bins))+
+  xlab("FPKM values")+
+  ylab("Number of variants")+
+  scale_fill_discrete(name = "Variant found in", labels = c("DNA", "DNA + RNA"))+
+  ggtitle("Variants found in DNA vs variants found in both DNA and RNA (filtered)")+
+  geom_bar(position="fill")+
+  geom_text(aes(label = ..count..), stat = "count", position="fill", vjust =1.5 )+
+  scale_x_discrete(guide=guide_axis(n.dodge=2))
+  
+
+#For unfiltered rna data.
+dna_exons<- dna_vu %>% 
+  filter(Location=="Exon")
+
+rna_exons_no_germ %>% 
+  mutate(Match="T")->rna_exons_no_germ
+
+dna_exons %>% 
+  left_join(rna_exons_no_germ) %>%
+  mutate(FPKM=as.numeric(FPKM)) %>% 
+  mutate_at(vars(Match), ~replace_na(., "F")) %>% 
+  drop_na(FPKM)->dna_exons
+
+dna_exons
+
+dna_exons$Match<-as.factor(dna_exons$Match)
+#maximum_FPKM<-max(dna_exons$FPKM)
+dna_exons$bins<-cut(dna_exons$FPKM, breaks=seq(from=0, to=ceiling(max(dna_exons$FPKM)), by=500))
+
+#dna_exons %>% 
+#  mutate(bins=cut(FPKM, breaks=seq(from=0, to=ceiling(max(FPKM)), by=500)))
+
+#Not percentages.
+dna_exons %>% 
+  drop_na(FPKM) %>%
+  drop_na(bins) %>% 
+  ggplot(aes(fill=Match, x=bins))+
+  xlab("ranges of FPKM values")+
+  ylab("Number of variants")+
+  scale_fill_discrete(name = "Variant found in", labels = c("DNA", "DNA + RNA"))+
+  ggtitle("Variants found in DNA vs variants found in both DNA and RNA")+
+  #xlim(-1,501)+
+  geom_bar()
+
+
+#Only look at counts for fpkm <500
+
+dna_exons_filtered<- dna_exons %>%
+  select(!bins) %>% 
+  filter(FPKM<=500)
+
+dna_exons_filtered$bins<-cut(dna_exons_filtered$FPKM, breaks=seq(from=0, to=500, by=50))
+#dna_exons_filtered
+dna_exons_filtered %>% 
+  drop_na(bins)->dna_exons_filtered_no_na
+
+ggplot(dna_exons_filtered_no_na, aes(fill=Match, x=bins))+
+  xlab("FPKM values")+
+  ylab("Number of variants")+
+  scale_fill_discrete(name = "Variant found in", labels = c("DNA", "DNA + RNA"))+
+  ggtitle("Variants found in DNA vs variants found in both DNA and RNA (unfiltered)")+
+  geom_bar(position="fill")+
+  geom_text(aes(label = ..count..), stat = "count", position="fill", vjust =1.5 )+
+  scale_x_discrete(guide=guide_axis(n.dodge=2))
+#remove the position="fill" to not have percentage on y axis but counts.
