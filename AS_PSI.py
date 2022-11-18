991b9288073dfb30bcf033f9086f87d7f3937bf6
Mirjam Karlsson-Müller <90756078+MirjamKarlssonMueller@users.noreply.github.com>
2022-11-18 14:36:37 +0100

    PSI scores for CE implemented

--- AS_PSI.py
+++ AS_PSI.py
@@ -179,7 +179,7 @@ def add_to(events):
         
     
 
-def PSI_CE(sample, triplet):
+def PSI_CE(sample, CE, gene):
     """
     
 
@@ -187,8 +187,8 @@ def PSI_CE(sample, triplet):
     ----------
     sample : string
         Sample name
-    exon : nested list
-        contains chromosome, start, stop, strand information for 3 exons.
+    exon : list
+        contains chromosome, start, stop, strand information for CE
 
     Returns
     -------
@@ -196,29 +196,149 @@ def PSI_CE(sample, triplet):
         PSI score
 
     """
-    #Needs coordinates for previous, CE and next exon.
-    previous= triplet[0]
-    CE= triplet[1]
-    following=triplet[2]
-    #A= Need number of reads within CE.
+    #Needs coordinates for CE
+    chrom=CE[0]
+    start=int(CE[1])
+    stop=int(CE[2])
+    strand=CE[3]
+    #print(start, stop)
     
-    #B= Number of reads within previous exon.
+    #Find .bam file corresponding to sample name.
+    for file in bam_file_list:
+        if sample in file:
+            bam_file=file
+            index_file=file[0:-1]+"i"
+            break
     
-    #C= number of reads within next exon.
+    #Initialize opening of file
+    samfile=pysam.AlignmentFile(bam_file, 'rb', index_filename=index_file)
     
-    #D= Number of spliced reads from previous to CE.
+    #C= number of reads within CE.
+    #For this we fetch the part of the alignment file in the exon.
+    exon_reads=samfile.fetch(chrom, start, stop)
+    #The coordinates do not need to be adjusted as pysam uses also start incl and end excl and 0 based.
     
-    #E= Number of spliced reads from CE to next.
+    read_dict=dict()
+    counter=0
+    for read in exon_reads:
+        #Filter
+        # Exclude non-primary alignments
+        if read.is_secondary:
+            continue
+        #exclude second read of pair, if maps to overlapping region.
+        read_name=read.query_name
+        read_start=int(read.reference_start)
+        read_length=int(read.infer_query_length())
+        if read_name in read_dict:
+            if read_dict[read_name][0]<=read_start<=read_dict[read_name][1] or \
+            read_dict[read_name][0]<=read_start+read_length<=read_dict[read_name][1]:
+                continue
+        else:
+            read_dict[read_name]=[read_start, read_start+read_length] 
+        counter+=1
     
-    #F= number of spliced reads accross CE.
+    #counter needs to be normalized with length of the exon.
+    C=counter/(stop-start)
+    #print(counter)
+    #quit()
     
-    # (A/length_CE) + D+ E/ ( A/length_CE + D + E + B/length_previous+ C/length_next+ F)
+    #For the spliced reads, we need to fetch a bigger region and then recognise the spliced alignments.
+    #We dont want to miss any reads, so we make this region the gene range the exon is in.
+    spliced_reads=samfile.fetch(chrom, gene_ranges[gene][0], gene_ranges[gene][1])
     
+    #initialize counters
+    counter_left=0
+    counter_right=0
+    counter_accross=0
+    #reset read dictionary
+    read_dict=dict()
     
+    for read in spliced_reads:
+        #only spliced reads
+        if not re.search(r'\d+M\d+N\d+M',read.cigarstring):
+            continue
+        #exclude second read of pair, if maps to overlapping region.
+        read_name=read.query_name
+        read_start=int(read.reference_start)
+        read_length=int(read.infer_query_length())
+        if read_name in read_dict:
+            if read_dict[read_name][0]<=read_start<=read_dict[read_name][1] or \
+            read_dict[read_name][0]<=read_start+read_length<=read_dict[read_name][1]:
+                continue
+        else:
+            read_dict[read_name]=[read_start, read_start+read_length] 
+        
+        "Get strand information, exclude reads on wrong strand"
+        if read.mate_is_reverse and read.is_read1:
+            read_strand="-"
+        elif read.mate_is_reverse and read.is_read2:
+            read_strand="+"
+        elif read.mate_is_forward and read.is_read1:
+            read_strand="+"
+        elif read.mate_is_forward and read.is_read2:
+            read_strand="-"
+        
+        if read_strand!=strand:
+            continue
+        
+        #Allow for several splice junctions in one read.
+        current_cigar = read.cigarstring
+        current_start = int(read_start)
+        #Do not process spliced reads in the wrong place.
+        """
+        if read_start < start and read_start+read_length < start:
+            continue
+        if read_start > stop and read_start+read_length > stop:
+            continue
+        """
+        while re.search(r'\d+M\d+N\d+M', current_cigar):
+            #assign splice junction variables
+            junction = re.search(r'(\d+)M(\d+)N(\d+)M', current_cigar)
+            exon1 = int(junction.group(1))
+            intron = int(junction.group(2))
+            exon2 = int(junction.group(3))
+            exon1_start = current_start
+            exon1_end = exon1_start+exon1+1  #exclusive
+            exon2_start = exon1_end+intron -1 #inclusive
+            exon2_end=exon2_start+exon2+1
+                
+            #skip alignments with less than 3 matching bases in an exon.
+            if exon1<3 or exon2<3:
+                # update cigar string
+                current_cigar = re.sub(r'^.*?N', 'N', current_cigar).lstrip("N")
+                current_start= exon2_start
+                continue
+            
+            #if exon1_end < start:
+            #    print(exon2_start, start)
+            
+            #D= Number of spliced reads from previous to CE.
+            #print(exon2_start, start)
+            if exon1_end< start and stop >= exon2_end>=start:
+                #print(exon1_end, exon2_start)
+                counter_left+=1
+            #E= Number of spliced reads from CE to next.   
+            elif start <= exon1_start<=stop and exon2_start >stop:
+                counter_right+=1
+                #print(exon1_end, exon2_start)
+            #F= number of spliced reads accross CE.
+            elif exon1_end<start and exon2_start>stop:
+                counter_accross+=1
+                #print(exon1_end, exon2_start)
+                
+            
+            # update cigar string
+            current_cigar = re.sub(r'^.*?N', 'N', current_cigar).lstrip("N")
+            current_start= exon2_start
     
+    IR= counter_left+counter_right+C
+    ER= counter_accross
+    if ER+ IR !=0:
+        PSI=str(round(IR/(IR+ER),3))
+    else:
+        PSI="NAN"
     
-    
-    return "NAN"
+    return PSI
 
 def PSI_AA(sample, exon):
     """
@@ -337,6 +457,7 @@ for file in [args.gencode, args.refseq]:
                     #add transcript ID to gene_dict dictionary
                     gene_dict[gene_name][trans_ID]=[]
                 
+                
                 #make entries for each exon.
                 for i in range(0, number_exons-1):
                     if i==0:
@@ -357,6 +478,19 @@ for file in [args.gencode, args.refseq]:
                                                            strand, position,db])
 
 print("Creating Database Dictionary: Done! \n", end="\r")
+
+#Make gene ranges dictionary to extract right read region later.
+gene_ranges=dict()
+for gene in gene_dict:
+    starts=[]
+    stops=[]
+    for trans_ID in gene_dict[gene]:
+        starts.append(int(gene_dict[gene][trans_ID][0][1]))
+        stops.append(int(gene_dict[gene][trans_ID][-1][2]))
+    #Take smallest start and biggest stop as range of gene.
+    gene_ranges[gene]=[min(starts), max(stops)]
+
+
 
 #%% SAMPLE NAMES?
 
@@ -382,6 +516,7 @@ for event in inputs:
             filtered_gene_dict[gene]={k : v for k , v in gene_dict[gene].items() 
                                 if len(v) >2}
         
+        """
         #For each potential CE, we need coordinates of previous, CE and following exon.
         
         draft_CE_dict=dict()
@@ -461,6 +596,8 @@ for event in inputs:
                         else:
                             exons_db["_".join(exon[0:-1])]=exon[-1]
             
+        
+        out=open(args.out+"PSI_CE.tsv", "w")
         #Make header for output file
         out.write("#PSI Table CE \n")
         out.write("Location\t"+"\t".join(sample_names)+"\n")
@@ -479,12 +616,13 @@ for event in inputs:
                 strand=exon[3]
                 
                 for sample in sample_names:
-                    PSI_scores.append(PSI_CE(sample, entry))
+                    PSI_scores.append(PSI_CE(sample, entry, gene))
+                    
                 
                 out.write("_".join(entry[0:3])+"\t" +"\t".join(PSI_scores)+"\n")
         
         out.close()
-    """
+    
     
     
     #"Alternative Donors/Acceptors"
